ac8f85e792671846d7e8f63a69c4a61897d3611b
Pedro Maita <Desarrollo2>
2017-03-13 16:39:57 -0500

    first commit

---
+++ Toolbar/app/src/main/java/academiasmoviles/com/toolbar/MainActivity.java
@@ -0,0 +1,57 @@
+package academiasmoviles.com.toolbar;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setToolbar();
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setContentInsetsAbsolute(0, 0);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setHomeButtonEnabled(false);
+            ab.setDisplayHomeAsUpEnabled(false);
+            ab.setDisplayShowTitleEnabled(false);
+        }
+        toolbar.setTitle("Academias Móviles");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuSearch:
+
+                Toast.makeText(getApplicationContext(), "Buscar", Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.menuSettings:
+
+                Toast.makeText(getApplicationContext(), "Configuración", Toast.LENGTH_SHORT).show();
+
+                break;
+        }
+        return true;
+    }
+
+}
